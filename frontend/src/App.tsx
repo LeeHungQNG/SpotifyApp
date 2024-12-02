2d3a80fd5d5ad1f011b196de6379bbc0ca88fd35
LeeHungQNG <builequochung3004@gmail.com>
2024-12-02 09:29:39 +0700

    setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button variant={'outline'}>This is a button</Button>
+    </>
+  );
+}
+
+export default App;
