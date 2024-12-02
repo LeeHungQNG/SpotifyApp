9cfbad96af473d3397bbd7eae101e1116aa6c6b7
LeeHungQNG <builequochung3004@gmail.com>
2024-12-02 09:40:48 +0700

    setup auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 import { Button } from './components/ui/button';
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button variant={'outline'}>This is a button</Button>
+      <header>
+        <SignedOut>
+          <SignInButton>
+            <Button>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
     </>
   );
 }
