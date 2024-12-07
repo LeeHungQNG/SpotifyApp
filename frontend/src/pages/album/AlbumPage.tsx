fe162eae9d1f2e5cf3002e09efd7a00ebf4f3534
LeeHungQNG <builequochung3004@gmail.com>
2024-12-07 15:33:55 +0700

    handle play song

--- frontend/src/pages/album/AlbumPage.tsx
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -1,7 +1,8 @@
 import { Button } from '@/components/ui/button';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { useMusicStore } from '@/stores/useMusicStore';
-import { Clock, Play } from 'lucide-react';
+import { usePlayerStore } from '@/stores/usePlayerStore';
+import { Clock, Pause, Play } from 'lucide-react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
@@ -14,6 +15,7 @@ const formatDuration = (seconds: number) => {
 const AlbumPage = () => {
   const { albumId } = useParams();
   const { fetchAlbumById, currentAlbum, isLoading } = useMusicStore();
+  const { currentSong, isPlaying, playAlbum, togglePlay } = usePlayerStore();
 
   useEffect(() => {
     if (albumId) {
@@ -23,6 +25,23 @@ const AlbumPage = () => {
 
   if (isLoading) return null;
 
+  const handlePlayAlbum = () => {
+    if (!currentAlbum) return;
+
+    const isCurrentAlbumPlaying = currentAlbum?.songs.some((song) => song._id === currentSong?._id);
+    if (isCurrentAlbumPlaying) togglePlay();
+    else {
+      // start playing the album from the beginning
+      playAlbum(currentAlbum?.songs, 0);
+    }
+  };
+
+  const handlePlaySong = (index: number) => {
+    if (!currentAlbum) return;
+
+    playAlbum(currentAlbum?.songs, index);
+  };
+
   return (
     <div className="h-full">
       <ScrollArea className="h-full rounded-md">
@@ -31,7 +50,7 @@ const AlbumPage = () => {
           {/* bg gradient */}
           <div
             className="absolute inset-0 bg-gradient-to-b from-[#5038a0]/80 via-zinc-900/80
-       to-zinc-900 pointer-events-none"
+					 to-zinc-900 pointer-events-none"
             aria-hidden="true"
           />
 
@@ -53,17 +72,16 @@ const AlbumPage = () => {
             {/* play button */}
             <div className="px-6 pb-4 flex items-center gap-6">
               <Button
-                // onClick={handlePlayAlbum}
+                onClick={handlePlayAlbum}
                 size="icon"
                 className="w-14 h-14 rounded-full bg-green-500 hover:bg-green-400 
-            hover:scale-105 transition-all"
+                hover:scale-105 transition-all"
               >
-                {/* {isPlaying && currentAlbum?.songs.some((song) => song._id === currentSong?._id) ? (
+                {isPlaying && currentAlbum?.songs.some((song) => song._id === currentSong?._id) ? (
                   <Pause className="h-7 w-7 text-black" />
                 ) : (
                   <Play className="h-7 w-7 text-black" />
-                )} */}
-                <Play className="h-7 w-7 text-black" />
+                )}
               </Button>
             </div>
 
@@ -72,7 +90,7 @@ const AlbumPage = () => {
               {/* table header */}
               <div
                 className="grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-10 py-2 text-sm 
-        text-zinc-400 border-b border-white/5"
+            text-zinc-400 border-b border-white/5"
               >
                 <div>#</div>
                 <div>Title</div>
@@ -87,20 +105,18 @@ const AlbumPage = () => {
               <div className="px-6">
                 <div className="space-y-2 py-4">
                   {currentAlbum?.songs.map((song, index) => {
-                    // const isCurrentSong = currentSong?._id === song._id;
+                    const isCurrentSong = currentSong?._id === song._id;
                     return (
                       <div
                         key={song._id}
-                        // onClick={() => handlePlaySong(index)}
+                        onClick={() => handlePlaySong(index)}
                         className={`grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-4 py-2 text-sm 
-                  text-zinc-400 hover:bg-white/5 rounded-md group cursor-pointer
-                  `}
+                      text-zinc-400 hover:bg-white/5 rounded-md group cursor-pointer
+                      `}
                       >
                         <div className="flex items-center justify-center">
-                          {/* {isCurrentSong && isPlaying ? <div className="size-4 text-green-500">♫</div> : <span className="group-hover:hidden">{index + 1}</span>}
-                          {!isCurrentSong && <Play className="h-4 w-4 hidden group-hover:block" />} */}
-                          <span className="group-hover:hidden">{index + 1}</span>
-                          <Play className="h-4 w-4 hidden group-hover:block" />
+                          {isCurrentSong && isPlaying ? <div className="size-4 text-green-500">♫</div> : <span className="group-hover:hidden">{index + 1}</span>}
+                          {!isCurrentSong && <Play className="h-4 w-4 hidden group-hover:block" />}
                         </div>
 
                         <div className="flex items-center gap-3">
